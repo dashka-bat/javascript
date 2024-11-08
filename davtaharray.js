6897928aed2af2b2a3e1626656c3d7093e85dd9b
dashka-bat <dushkab02@gmail.com>
2024-11-08 10:18:21 +0800

    array2

--- davtaharray.js
+++ davtaharray.js
@@ -33,17 +33,19 @@
 ////5))))
 ////1))))
 numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-numberssum=0
-for(i=0;i<numbers.length;i++){
-    numberssum=numbers[i]+numberssum
-}
-console.log(numberssum)
+// numberssum=0
+// for(i=0;i<numbers.length;i++){
+//     numberssum=numbers[i]+numberssum
+// }
+// console.log(numberssum)
 ////2))))
-Hamginih=0
+Hamginih=index[0]
 for(i=0;i<numbers.length;i++){
-for(a=0;a<numbers.length;a++){
-    Hamginih=numbers[a]<numbers[i]
+if(numbers[i]>Hamginih){
+    Hamginih=numbres[i]
 }
+
+    
 }
 
 console.log(Hamginih)
